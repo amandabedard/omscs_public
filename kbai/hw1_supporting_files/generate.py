cc53c735dd47f76a2501b6b2cc07e18b1698de85
Amanda Bedard <bedard.amanda6@gmail.com>
2026-02-06 21:13:17 -0500

    fix: come up with a plan

---
+++ kbai/hw1_supporting_files/generate.py
@@ -0,0 +1,19 @@
+# Creating code to generate one state at a time
+
+# Our state object. We can use this to test the generator
+example = {
+    "frodo": "LEFT",
+    "ring": "LEFT",
+    "sam": "LEFT",
+    "gollum": "LEFT"
+}
+
+# The initial state generator. We are focused on only generating valid transition states, not
+# checking the results. That's for test.py
+def generate(initial_state:dict):
+    # The rules:
+    # 1. Sam must always flip sides (he steers the boat)
+    # 2. Frodo and Gollum cannot be on the boat with the ring
+    # (so the ring will be treated as its own entity)
+    # 3. Frodo and Gollum cannot drive the boat
+    return 
