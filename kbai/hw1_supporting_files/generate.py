96a5753c3c2348ede9f29871ae2f1a59201f6e5a
Amanda Bedard <bedard.amanda6@gmail.com>
2026-02-06 21:35:28 -0500

    generate completed, starting test

--- kbai/hw1_supporting_files/generate.py
+++ kbai/hw1_supporting_files/generate.py
@@ -11,7 +11,8 @@ example = {
 # The initial state generator. We are focused on only generating valid transition states, not
 # checking the results. That's for test.py
 def generate(initial_state:dict) -> tuple:
-    transition_states = []
+    # Sam can always go by himself
+    transition_states = [("sam",)]
     # The rules:
     # 1. Sam must always flip sides (he steers the boat)
     # 2. Frodo and Gollum cannot be on the boat with the ring
@@ -19,5 +20,20 @@ def generate(initial_state:dict) -> tuple:
     # 3. Frodo and Gollum cannot drive the boat
 
     # Sam is the key piece in this decision
+    sam_state = initial_state["sam"]
+
+    # Basically, any valid transition is sam + another entity, or
+    # sam on his own depending on the initial state.
+    if sam_state == initial_state["ring"]:
+        # Sam can take the ring
+        transition_states.append(("sam", "ring"))
+    if sam_state == initial_state["frodo"]:
+        # Sam can take frodo
+        transition_states.append(("sam", "frodo"))
+    if sam_state == initial_state["gollum"]:
+        # Sam can take gollum
+        transition_states.append(("sam", "gollum"))
     
+    
+    print(f"Discovered the following transition states: {transition_states}")
     return transition_states
--- kbai/hw1_supporting_files/test.py
+++ kbai/hw1_supporting_files/test.py
@@ -1,8 +1,17 @@
 # We will take the initial state and a transition tuple to build the new state
 # and make sure it is not invalid
-def test(initial_state, transition_list:list[dict]):
+def test(initial_state:dict, transition_list:list[tuple]) -> list[dict]:
     # We will take the list of generated states and check them with the test function against the rules
     # we know. They are:
     # 1. Gollum nor Frodo can be alone with the Ring
     filtered_list = []
     return filtered_list
+
+def _apply(initial_state:dict, transition_list:list[tuple]):
+    # We will apply the transition to the state
+    return
+
+def _is_valid(state: dict) -> bool:
+    # Is this a valid state? We will go through conditions to determine
+    valid = True
+    return valid
